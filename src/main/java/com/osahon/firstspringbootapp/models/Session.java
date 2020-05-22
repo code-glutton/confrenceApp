dc019612e632a374c3a428b49b5faade6f175f4f
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-05-22 22:44:17 +0100

    speaker entity created

--- src/main/java/com/osahon/firstspringbootapp/models/Session.java
+++ src/main/java/com/osahon/firstspringbootapp/models/Session.java
@@ -1,11 +1,52 @@
 package com.osahon.firstspringbootapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity(name = "sessions")
 public class Session {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+    private String session_name;
+    private String session_description;
+    private Integer session_length;
 
     public Session() {
+    }
+
+    public Long getSession_id() {
+        return session_id;
+    }
+
+    public void setSession_id(Long session_id) {
+        this.session_id = session_id;
+    }
+
+    public String getSession_name() {
+        return session_name;
+    }
+
+    public void setSession_name(String session_name) {
+        this.session_name = session_name;
+    }
+
+    public String getSession_description() {
+        return session_description;
+    }
+
+    public void setSession_description(String session_description) {
+        this.session_description = session_description;
+    }
+
+    public Integer getSession_length() {
+        return session_length;
+    }
+
+    public void setSession_length(Integer session_length) {
+        this.session_length = session_length;
     }
 }
