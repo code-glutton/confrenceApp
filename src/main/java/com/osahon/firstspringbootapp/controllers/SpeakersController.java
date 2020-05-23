0105d062adeb2c96b81aabe71e9d6d979ba72b61
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-05-23 23:09:50 +0100

    speaker Rest API controller added

--- src/main/java/com/osahon/firstspringbootapp/controllers/SpeakersController.java
+++ src/main/java/com/osahon/firstspringbootapp/controllers/SpeakersController.java
@@ -1,7 +1,7 @@
 package com.osahon.firstspringbootapp.controllers;
 
-import com.osahon.firstspringbootapp.models.Session;
-import com.osahon.firstspringbootapp.repositories.SessionRepository;
+import com.osahon.firstspringbootapp.models.Speaker;
+import com.osahon.firstspringbootapp.repositories.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("conferenceapi/v1/sessions")
-public class SessionsController {
+@RequestMapping("conferenceapi/v1/speakers")
+public class SpeakersController {
     @Autowired
-    private SessionRepository sessionRepository;
+    private SpeakerRepository speakerRepository;
 
     @GetMapping
-    public List<Session> list(){
-        return sessionRepository.findAll();
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
     }
 
     @GetMapping
     @RequestMapping("{id}")
-    public Session get(@PathVariable Long id){
-        return sessionRepository.getOne(id);
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
     }
 }
