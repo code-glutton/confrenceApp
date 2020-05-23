a61cfdf3fd684737e5fc85141200a6c80d90f629
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-05-24 00:01:46 +0100

    the speaker controller has been set up

--- src/main/java/com/osahon/firstspringbootapp/controllers/SessionsController.java
+++ src/main/java/com/osahon/firstspringbootapp/controllers/SessionsController.java
@@ -2,11 +2,9 @@ package com.osahon.firstspringbootapp.controllers;
 
 import com.osahon.firstspringbootapp.models.Session;
 import com.osahon.firstspringbootapp.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,23 @@ public class SessionsController {
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id){
         return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session,existingSession,"session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 }
--- src/main/java/com/osahon/firstspringbootapp/controllers/SpeakersController.java
+++ src/main/java/com/osahon/firstspringbootapp/controllers/SpeakersController.java
@@ -1,12 +1,11 @@
 package com.osahon.firstspringbootapp.controllers;
 
+import com.osahon.firstspringbootapp.models.Session;
 import com.osahon.firstspringbootapp.models.Speaker;
 import com.osahon.firstspringbootapp.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,22 @@ public class SpeakersController {
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id){
         return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
     }
 }
